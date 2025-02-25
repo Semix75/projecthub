e6bb2b3c2323324b1c943f9c1691ea5230c981fc
adel-212 <adelmoussaoui04@gmail.com>
2025-02-25 14:29:02 +0100

    ajout dashboard admin, inscription, connexion user, bootstrap

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Acceuil;
+use App\Entity\Art;
+use App\Entity\Gallerie;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\AcceuilFormType;
+use App\Entity\Contenu;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
+// Add missing use statement
+
+class DashboardController extends AbstractDashboardController
+{
+   
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        //return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/index.html.twig');
+        }
+    
+        return $this->redirectToRoute('app_login');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('ProjectHub');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');        
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+
+    }
+
+}
---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new(propertyName: 'email'),
+            ChoiceField::new('roles')->allowMultipleChoices()->setChoices(['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_USER' => 'ROLE_USER']),
+            TextField::new(propertyName: 'firstname'),
+            TextField::new(propertyName: 'lastname'),
+
+        ];
+    }
+    
+}
