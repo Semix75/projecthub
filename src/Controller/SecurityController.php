1de0cebd84ed5f87dc8b51a137577c4fb71715f4
Amel Bakouri <amel.bakouri@etu.univ-amu.fr>
2025-02-27 16:09:41 +0100

    vue + formulaire projets

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,17 +12,20 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_etudiant'); // Redirige vers /etudiant si déjà connecté
+        }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
+
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
