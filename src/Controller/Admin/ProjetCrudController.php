39d3d5675c8e8ebb1b24f433aec2e721fa45be44
adel-212 <adelmoussaoui04@gmail.com>
2025-03-11 11:22:13 +0100

    modif crud

--- src/Controller/Admin/ProjetCrudController.php
+++ src/Controller/Admin/ProjetCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Projet;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +16,17 @@ class ProjetCrudController extends AbstractCrudController
         return Projet::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('intitule'),
             TextEditorField::new('description'),
+            NumberField::new('nbPlaceMax'),
+            NumberField::new('nbPlaceMin'),
+
         ];
     }
-    */
+    
 }
