a6d4ae9611b085b3c8a8809a128f290a260dbd5e
Amel Bakouri <amel.bakouri@etu.univ-amu.fr>
2025-03-21 05:04:25 +0100

    fix pb modif profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\ProfilType;
+use App\Form\ProfilType; 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
