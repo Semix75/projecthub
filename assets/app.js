83b60317f2dfb0fd567564304620523427c2c961
adel-212 <adelmoussaoui04@gmail.com>
2025-03-25 15:24:17 +0100

    init

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
 import './styles/app.scss';
-import './assets/js/scripts.js';
+import './js/scripts.js';
 import 'bootstrap'; // Import des fonctionnalités JavaScript de Bootstrap
 console.log('Hello from app.js');
