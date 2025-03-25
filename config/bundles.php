0e947ba8dc4e22e603508615c795af157ed63e30
Amel Bakouri <amel.bakouri@etu.univ-amu.fr>
2025-03-25 02:13:19 +0100

    l

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Artprima\PrometheusMetricsBundle\ArtprimaPrometheusMetricsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
