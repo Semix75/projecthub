b093fb141b07214803447eac1c8f6ffeed4edfb8
Amel Bakouri <amel.bakouri@etu.univ-amu.fr>
2025-03-27 21:48:21 +0100

    modif leger

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -9,54 +9,47 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Twig\Environment;
+
 
 class ExceptionListener
 {
     private RouterInterface $router;
     private AuthorizationCheckerInterface $authChecker;
     private RequestStack $requestStack;
-    private Environment $twig;
 
     public function __construct(
         RouterInterface $router,
         AuthorizationCheckerInterface $authChecker,
         RequestStack $requestStack,
-        Environment $twig
     ) {
         $this->router = $router;
         $this->authChecker = $authChecker;
         $this->requestStack = $requestStack;
-        $this->twig = $twig;
     }
 
     public function onKernelException(ExceptionEvent $event)
     {
-        // $exception = $event->getThrowable();
-        // $request = $this->requestStack->getCurrentRequest();
+        $exception = $event->getThrowable();
+        $request = $this->requestStack->getCurrentRequest();
 
-        // // Éviter une boucle infinie de redirections
-        // $currentRoute = $request->attributes->get('_route');
+        // Éviter une boucle infinie de redirections
+        $currentRoute = $request->attributes->get('_route');
 
-        // if ($exception instanceof NotFoundHttpException) {
-        //     if ($currentRoute === 'app_error_404') {
-        //         return;
-        //     }
+        if ($exception instanceof NotFoundHttpException) {
+            if ($currentRoute === 'app_error_404') {
+                return;
+            }
 
-            // Option 1 : Redirection vers une page d'erreur (décommenter pour activer)
-            //$event->setResponse(new RedirectResponse($this->router->generate('app_error_404')));
-            //return;
-
-            // Option 2 : Réponse directe avec le template 404 (recommandé)
-            // $response = new Response($this->twig->render('errors/404.html.twig'), 404);
-            // $event->setResponse($response);
-            // return;
+            // Option 1 : Redirection vers une page d'erreur 404
+            $event->setResponse(new RedirectResponse($this->router->generate('app_error_404')));
+            return;
         }
 
-        // if ($currentRoute === 'app_error_general') {
-        //     return;
-        // }
+        if ($currentRoute === 'app_error_general') {
+            return;
+        }
 
         // Redirection vers la page d'erreur générale
-        // $event->setResponse(new RedirectResponse($this->router->generate('app_error_general')));
+        $event->setResponse(new RedirectResponse($this->router->generate('app_error_general')));
     }
+}
